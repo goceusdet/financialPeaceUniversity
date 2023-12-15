4f90b2e0b6da5def1e9366020215b99437dfc920
goceusdet <gocesdet@gmail.com>
2023-12-14 19:40:37 -0500

    Created Main Runner class.

---
+++ src/test/java/com/financialPeaceUniversity/runners/MainRunner.java
@@ -0,0 +1,19 @@
+package com.financialPeaceUniversity.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {
+                "me.jvt.cucumber.report.PrettyReports:target/cucumber",
+                "html:target/cucumber-reports.html"
+        },
+        features = "src/test/resources/features",
+        glue = "com/financialPeaceUniversity/stepDefinitions",
+        dryRun = true,
+        tags = "",
+        publish = true
+)
+public class MainRunner {}
